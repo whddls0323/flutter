88cd3c917896cffd8737af7a05f31899391842d4
whddls0323 <whddls0323@naver.com>
2025-10-24 10:17:30 +0900

    add ch01

--- ch01/10_generic_collections.dart
+++ ch01/10_generic_collections.dart
@@ -1,17 +1,125 @@
 /*
   날짜: 2025/10/23
   이름: 윤종인
-  내용: 1장 Flutter 개발환경 구축
-
-  - Android Studio 설치
-  - Flutter SDK 다운로드 및 압축 해제
-  - Android Studio > plugins > flutter, dart 플러그인 설치
+  내용: Dart 제네릭 컬렉션
  */
 
-import 'dart:io';
+class Box<T> {
+  T item;
+
+  Box(this.item);
+
+  T getItem() {
+    return item;
+  }
+}
 
 void main() {
-  print('Hello World!');
-  print('Hello Dart!');
-  print('Dart Version: ${Platform.version}');
+  //제네릭 객체 생성
+  Box stringBox = Box<String>('hello');
+  String text = stringBox.getItem();
+  print('text : $text');
+
+  Box intBox = Box<int>(100);
+  int num = intBox.getItem();
+  print('num: $num');
+
+  /////////////////////////////////////////////////
+  //List
+  /////////////////////////////////////////////////
+  //List 생성
+  List<String> fruits = ['Apple','Banana','Cherry'];
+  print('fruits: $fruits');
+
+  //요소 추가
+  fruits.add('Durian');
+  print('fruits: $fruits');
+
+  //요소 참조
+  print('첫번째 과일: ${fruits[0]}');
+  print('첫번째 과일: ${fruits.first}');
+  print('마지막 과일: ${fruits.last}');
+
+  //원소 수정
+  fruits[1] = 'Berry';
+  print('fruits: $fruits');
+
+  //원소 제거
+  String removedFruit = fruits.removeAt(0);
+  print('removed: $removedFruit');
+  print('fruits: $fruits');
+
+  //리스트 크기
+  print('과일 갯수: ${fruits.length}');
+
+  //리스트 반복문
+  for(String fruit in fruits) {
+    print('과일: $fruit');
+  }
+
+  //리스트 필터링
+  List<int> numbers = [1,2,3,4,5];
+  var evenNumbers = numbers.where((n) => n % 2 == 0);
+  print('짝수: $evenNumbers');
+
+  //변환
+  var doubleList = numbers.map((n) => n * 2).toList();
+  print('doubleList: $doubleList');
+
+  //정렬
+  numbers.sort();
+  print('오름차순: $numbers');
+  
+  numbers.sort((a,b) => b.compareTo(a));
+  print('내림차순: $numbers');
+
+  //반복처리
+  numbers.forEach((n) => print('n값: $n'));
+
+  /////////////////////////////////////////////////
+  //Set, 중복 제거 자료구조
+  /////////////////////////////////////////////////
+  Set<String> colors = {'red','green','blue'};
+  print('colors: $colors');
+
+  //원소 추가
+  colors.add('orange');
+  colors.add('red'); //중복 안됨
+  print('colors: $colors');
+
+  //집한 연산(합집합)
+  var set1 = {1,2,3,4};
+  var set2 = {3,4,5,6};
+
+  Set<int> unionSet = set1.union(set2);
+  print('합집합: $unionSet');
+
+  //교집합
+  Set<int> intersectSet = set1.intersection(set2);
+  print('교집합: $intersectSet');
+  
+  //차집합
+  Set<int> differenceSet = set1.difference(set2);
+  print('차집합: $differenceSet');
+
+  /////////////////////////////////////////////////
+  //Map
+  /////////////////////////////////////////////////
+  Map<String, String> user = {
+    'id' : 'a101',
+    'name' : '홍길동',
+    'address' : '부산'
+  };
+  print('user: $user');
+
+  //Map 키 참조
+  print('이름: ${user['name']}');
+  print('주소: ${user['address']}');
+
+  //키 존재 여부
+  print('age 키 존재 여부: ${user.containsKey('age')}');
+
+  //모든 키, 값을 리스트 생성
+  print('모든 키 값 목록: ${user.keys.toList()}');
+  print('모든 값 값 목록: ${user.values.toList()}');
 }
--- ch01/11_future_async_await.dart
+++ ch01/11_future_async_await.dart
@@ -1,17 +1,84 @@
 /*
   날짜: 2025/10/23
   이름: 윤종인
-  내용: 1장 Flutter 개발환경 구축
-
-  - Android Studio 설치
-  - Flutter SDK 다운로드 및 압축 해제
-  - Android Studio > plugins > flutter, dart 플러그인 설치
+  내용: Dart 비동기 처리
  */
 
-import 'dart:io';
+//Future는 미래에 가져오는 값을 의미하는 객체
+import 'dart:async';
 
-void main() {
-  print('Hello World!');
-  print('Hello Dart!');
-  print('Dart Version: ${Platform.version}');
+Future<String> fetchData() {
+  //비동기 작업 정의
+  return Future.delayed(Duration(seconds: 3),(){ //Duration(seconds: 3) => 3초후에 (){}가 실행
+    return 'fetchData!';
+  });
+}
+
+//Stream을 반환하는 함수
+Stream<int> countStream() async* {
+  for(int i=1;i<=5;i++) {
+    await Future.delayed(Duration(seconds: 1));
+    yield i; //스트림으로 데이터는 내보내기(emit)
+  }
+}
+
+void main() async {
+  //Future 비동기 작업 수행
+  print('here...1');
+  
+  fetchData()
+      .then((data){
+        print('here...2: $data');
+      })
+      .catchError((err){
+        print('here...3: $err');
+      })
+      .whenComplete((){
+        print('here...4 비동기 작업 완료');
+  });
+
+  print('here...5');
+
+  //async/await 실행
+  print('async_await...1');
+
+  try {
+    String data = await fetchData();
+    print('async_await...2: $data');
+  } catch(e) {
+    print('async_await...3: $e');
+  } finally {
+    print('async_await...4 비동기 작업 완료');
+  }
+
+  print('async_await...5');
+
+  /*
+    Future vs Stream
+    - Future는 한 번만 비동기 값을 반환하는 비동기 처리
+    - Stream은 시간이 지나면서 여러 데이터를 순차적으로 전달하는 비동기 처리
+  */
+  //Stream 작업
+  final controller = StreamController<String>(); //String은 스트림으로 보내는 데이터 타입
+
+  //Stream 리스너 등록
+  controller.stream
+      .listen((data){
+        print('stream...1: $data');
+      })
+      .onError((err){
+        print('stream error');
+      });
+
+  //스트림 데이터 전송
+  controller.sink.add('hello');
+  controller.sink.add('welcome');
+  controller.sink.add('greeting');
+
+  //Stream 함수 호출 후 await for 출력
+  Stream<int> stream = countStream();
+
+  await for(int number in stream) {
+    print('스트림 데이터 수신: $number');
+  }
 }
